a1e2bbc8bf57e4b0e19e9ddb7545cb90651232f6
selvin-rivera <nrivera.clements@gmail.com>
2024-06-11 21:46:19 -0600

    Add project files.

---
+++ SumaAppMvvm/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SumaAppMvvm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
